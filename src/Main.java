013ac410f49430f0a7dc112854b6009bcbf3cb2f
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-08-17 14:38:40 +0600

    update: Move scanner initialization to class level for accessibility in deposit method

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         // declare variables
-        Scanner scanner = new Scanner(System.in);
         double balance;
         boolean isRunning = true;
 
@@ -63,6 +64,7 @@ public class Main {
         double amount;
 
         System.out.print("Enter the amount you want to deposit: €");
+        amount = scanner.nextDouble();
 
         return 0.0;
     }
